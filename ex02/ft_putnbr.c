49b80863e9fbeb64bf83396f72190172f4a2c748
asoler <asoler@student.42sp.org.br>
2022-02-16 20:12:56 +0100

    wip trying to print INT_MIN

--- ex02/ft_putnbr.c
+++ ex02/ft_putnbr.c
@@ -6,41 +6,66 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 03:39:52 by asoler            #+#    #+#             */
-/*   Updated: 2022/02/16 04:26:10 by asoler           ###   ########.fr       */
+/*   Updated: 2022/02/16 20:12:09 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putnbr(int nb)
+int	count_digits(int nb)
 {
-	int			i;
-	int		size;
-	char	*aux;
-	int		count;
+	int counter;
 
-	i = 0;
-	count = 0;
+	counter = 0;
 	while (nb > 9)
 	{
-		aux[i]= (nb % 10) - 48;
+		nb = nb % 10;
+		counter++;
+	}
+	return (counter);
+}
+
+void	ft_putnbr(int nb)
+{
+	char	aux;
+	int			i;
+	int size;
+	size = count_digits(nb);
+	char	digits[size+1];
+
+	i = 0;
+	
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		// if (nb == -2147483648)
+		// {
+		// 	nb = nb
+		// }
+		nb *= -1;
+	}
+	if (nb <= 9)
+	{
+		aux = nb + 48;
+		write(1, &aux, 1);
+	}
+
+	while (nb > 9)
+	{
+		digits[i] = (nb % 10) + 48;
 		nb = nb / 10;
 		i++;
 	}
-	aux[i]= nb - 48;
-	if (i != 0)
+	digits[i] = nb + 48;
+	while (i != 0)
 	{
-		size = i;
-		while (count < size)
-		{
-			write(1, &aux[i], 1);
-			count++;
-			i--;
-		}
+		write(1, &digits[i], 1);
+		i--;
 	}
+	write(1, &digits[i], 1);
 }
 
 int	main()
 {
-	ft_putnbr(156515);
+	ft_putnbr(-2147483649);
 }
