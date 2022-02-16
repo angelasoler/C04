6ec246286d5e547456bc3534adc6403e30de54d3
asoler <asoler@student.42sp.org.br>
2022-02-16 03:33:58 +0100

    making it the short way, without index

---
+++ ex01/ft_putstr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 03:28:50 by asoler            #+#    #+#             */
+/*   Updated: 2022/02/16 03:33:22 by asoler           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	main()
+{
+	char source[100] = "Me imprima caracter por caracter, obgd.\n";
+	ft_putstr(source);
+}
